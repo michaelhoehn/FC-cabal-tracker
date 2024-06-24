1737b3554df8ef023fee8f2ec4c717ae05605a7c
Michael Hoehn <michael.hoehn@burohappold.com>
2024-06-24 08:59:37 -0400

    change dune query to minimize api spend

--- src/services/duneService.js
+++ src/services/duneService.js
@@ -1,7 +1,7 @@
 export const fetchDuneData = async () => {
   const DUNE_API_KEY = process.env.REACT_APP_DUNE_API_KEY;
   const response = await fetch(
-    "https://api.dune.com/api/v1/query/3832587/results",
+    "https://api.dune.com/api/v1/query/3854565/results",
     {
       headers: {
         "X-Dune-API-Key": DUNE_API_KEY,
